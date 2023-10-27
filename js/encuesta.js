816d1efaec401d716fe60fd259734ad84ce505e5
kenny1303 <arevalosarmientokenny@gmail.com>
2023-10-27 09:33:38 -0500

    subidos

--- js/encuesta.js
+++ js/encuesta.js
@@ -110,6 +110,38 @@ function procesarDatos() {
 //     alert("¡GRACIAS POR TU APOYO!");
 // }
 
+var validarRadios = function() {
+    var opciones = document.getElementsByName('sexo'),
+      resultado = false;
+
+    for (var i = 0; i < elementos.length; i++) {
+      if (elementos[i].type == "radio" && elementos[i].name == "sexo") {
+        // Recorremos los radio button
+        for (var o = 0; o < opciones.length; o++) {
+          if (opciones[o].checked) {
+            resultado = true;
+            break;
+          }
+        }
+
+        if (resultado == false) {
+          elementos[i].parentNode.className = elementos[i].parentNode.className + " error";
+          console.log('El campo sexo esta incompleto');
+          return false;
+        } else {
+          // Eliminamos la clase Error del radio button
+          elementos[i].parentNode.className = elementos[i].parentNode.className.replace(" error", "");
+          return true;
+        }
+      }
+    }
+  };
+
+  function irAInicio() {
+    // Redirigir al usuario a la página de inicio
+    window.location.href="../index.html";
+  }
+
 
 
 
--- js/evaluacion.js
+++ js/evaluacion.js
@@ -72,3 +72,5 @@ window.onload = function() {
 }
 
 
+
+
